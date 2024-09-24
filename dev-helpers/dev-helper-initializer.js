1d9c0f445182e002f32b8e0347ed5d16654546c7
Aman Shah <aman.shah@incodus.in>
2024-09-24 18:35:45 +0530

    changed config

--- dev-helpers/dev-helper-initializer.js
+++ dev-helpers/dev-helper-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   window["SwaggerUIStandalonePreset"] = window["swagger-ui-standalone-preset"]
   // Build a system
   const ui = SwaggerUIBundle({
-    url: "../dist/openapi.json",
+    url: "../dist/openapi.yaml",
     dom_id: "#swagger-ui",
     presets: [
       SwaggerUIBundle.presets.apis,
